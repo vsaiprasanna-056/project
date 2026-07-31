e5c94298c8c0ee8293f2365f28363cfda1333794
vsaiprasanna-056 <saiprasannav999@gmail.com>
2026-07-31 11:23:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/products_db"
 DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_okX1RFsckRw31m5aeyz@mysqldb-saiprasannav999-dcd7.e.aivencloud.com:16816/defaultdb"
-engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl":{}
+    }
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
