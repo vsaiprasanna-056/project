332a71343f6cc265d1f6a7784729c0feef59a882
vsaiprasanna-056 <saiprasannav999@gmail.com>
2026-08-02 20:06:21 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -42,3 +42,34 @@ def verify_admin(request: Request):
             status_code=401,
             detail="Invalid token"
         )
+def verify_customer(request: Request):
+
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(
+            status_code=401,
+            detail="Not authenticated"
+        )
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=401,
+            detail="Token expired"
+        )
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
